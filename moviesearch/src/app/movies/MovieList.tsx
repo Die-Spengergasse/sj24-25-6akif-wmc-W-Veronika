f496481e9ef979523f571c9c94d317f3db033156
Veronika Wiesinger <wie230608@spengergasse.at>
2025-02-24 13:27:01 +0100

    Moviesearch works.

--- moviesearch/src/app/movies/MovieList.tsx
+++ moviesearch/src/app/movies/MovieList.tsx
@@ -12,12 +12,12 @@ export default function MovieList({ movies }: MovieListProps) {
             <h2>{movies.length} movies found</h2>
             <div className={styles.movies}>
                 {movies.map(movie => (
-                  <Link key={movie.imdbID} href={`/movies/${movie.imdbID}`}>
-                    <div key={movie.imdbID} className={styles.movie}>
-                        <div className={styles.moviePoster}><img src={movie.Poster} alt={movie.Title} /></div>
-                        <div className={styles.movieTitle}>{movie.Title}</div>
-                    </div>
-                  </Link>  
+                    <Link key={movie.imdbID} href={`/movies/${movie.imdbID}`}>
+                        <div key={movie.imdbID} className={styles.movie}>
+                            <div className={styles.moviePoster}><img src={movie.Poster} alt={movie.Title} /></div>
+                            <div className={styles.movieTitle}>{movie.Title}</div>
+                        </div>
+                    </Link>  
                 ))}
             </div>
         </div>
